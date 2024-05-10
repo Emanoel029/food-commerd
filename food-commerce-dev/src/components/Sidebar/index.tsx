aa25f9d45309912a567e9004f71dbc9bc2a349cf
Emanoel Pontes Timbo <emanoelpontes8@gmail.com>
2024-05-10 16:56:28 -0300

    Criando o Sidebar

---
+++ food-commerce-dev/src/components/Sidebar/index.tsx
@@ -0,0 +1,24 @@
+import { Container } from './styles'
+
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+
+import menuImg from '../../assets/menu.svg'
+export function Sidebar() {
+  return (
+    <Container>
+      <button type='button'>
+        <img src={menuImg} alt='Abrir e fechar o menu' />
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <a href='#' className='active'>
+              <BurgerIcon />
+              <span>Hambúrgueres</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  )
+}
