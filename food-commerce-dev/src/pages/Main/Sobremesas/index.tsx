fe8e387ae8e00f39b9e3a7c7cf440bc4d29fdbae
Emanoel Pontes Timbo <emanoelpontes8@gmail.com>
2024-05-13 19:32:23 -0300

    Criando as páginas e adicionando os componentes principais

---
+++ food-commerce-dev/src/pages/Main/Sobremesas/index.tsx
@@ -0,0 +1,11 @@
+import { Head } from '../../../components/Head'
+import { SnackTitle } from '../../../components/SnackTitle'
+
+export default function Sobremesas() {
+  return (
+    <>
+      <Head title='Sobremesas' />
+      <SnackTitle>Sobremesas</SnackTitle>
+    </>
+  )
+}
