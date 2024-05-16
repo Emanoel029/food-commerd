ebe6ae1780fb9bd57bbcc7a2f2a21f0d84f6d6b8
Emanoel Pontes Timbo <emanoelpontes8@gmail.com>
2024-05-16 18:53:31 -0300

    Criando Loading de dados

--- food-commerce-dev/src/services/api.ts
+++ food-commerce-dev/src/services/api.ts
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
+console.log('API Base URL:', process.env.REACT_APP_API_BASE_URL)
+
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 })
+
+export const getBurgers = () => api.get('/burgers')
+export const getPizzas = () => api.get('/pizzas')
+export const getDrinks = () => api.get('/drinks')
+export const getSobremesas = () => api.get('/sobremesas')
 
 export default api
